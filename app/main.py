88d8d3e63d8f3cd1d6fedb201eea60f8817b9b2e
박현서 <030409phs@gmail.com>
2026-05-21 19:20:21 +0900

    run postgre

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.database import engine
+from app.models.user import User
+from app.database import Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Trusty backend running"}
