31072cda2bf692966d3970eee11ba34d19521a95
박현서 <030409phs@gmail.com>
2026-07-18 13:45:02 +0900

    API키 입력

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -1,50 +1,70 @@
 import json
 
-# 나중에 Gemini 사용할 때 다시 주석 해제
-# from google import genai
-# from app.core.config import settings
+from google import genai
 
-# client = genai.Client(api_key=settings.GEMINI_API_KEY)
+from app.core.config import settings
+
+
+client = genai.Client(
+    api_key=settings.GEMINI_API_KEY
+)
 
 
 def analyze_text(text: str):
 
-    phishing_keywords = [
-        "계좌", "송금", "긴급", "검찰", "경찰", "금감원",
-        "인증", "링크", "당첨", "택배", "환급"
+    prompt = f"""
+당신은 보이스피싱 탐지 AI입니다.
+
+아래 문장을 분석하세요.
+
+문장:
+{text}
+
+반드시 아래 JSON 형식으로만 응답하세요.
+
+{{
+    "risk_level":"HIGH | MEDIUM | LOW",
+    "score":95,
+    "scam_type":"기관사칭",
+    "reason":"판단 이유",
+
+    "url_risk_score":85,
+    "language_pattern_score":90,
+    "sender_reliability_score":30,
+    "urgency_score":95,
+
+    "recommended_actions":[
+        "링크를 클릭하지 마세요",
+        "송금하지 마세요",
+        "경찰청 또는 금융감독원에 신고하세요"
     ]
+}}
 
-    if any(keyword in text for keyword in phishing_keywords):
-        return {
-            "risk_level": "HIGH",
-            "score": 92,
-            "scam_type": "기관사칭",
-            "reason": "피싱에서 자주 사용하는 키워드가 포함되어 있습니다.",
+설명은 절대 쓰지 말고 JSON만 출력하세요.
+"""
 
-            "url_risk_score": 80,
-            "language_pattern_score": 90,
-            "sender_reliability_score": 20,
-            "urgency_score": 95,
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
 
-            "recommended_actions": [
-                "링크를 클릭하지 마세요.",
-                "송금하지 마세요.",
-                "경찰청 또는 금융감독원에 신고하세요."
-            ]
-        }
+    if response.text is None:
+        raise Exception("Gemini 응답이 비어 있습니다.")
 
-    return {
-        "risk_level": "LOW",
-        "score": 15,
-        "scam_type": "정상",
-        "reason": "위험한 표현이 발견되지 않았습니다.",
+    response_text = response.text.strip()
 
-        "url_risk_score": 0,
-        "language_pattern_score": 10,
-        "sender_reliability_score": 90,
-        "urgency_score": 5,
+    # 코드블록 제거
+    response_text = (
+        response_text
+        .replace("```json", "")
+        .replace("```", "")
+        .strip()
+    )
 
-        "recommended_actions": [
-            "현재 특별한 위험 요소는 발견되지 않았습니다."
-        ]
-    }
+    try:
+        result = json.loads(response_text)
+    except Exception:
+        print(response_text)
+        raise Exception("Gemini가 JSON 형식으로 응답하지 않았습니다.")
+
+    return result
